481deef5576cf102ceabd9580540d5a11a690414
Rune K. Svendsen <runesvend@gmail.com>
2016-11-19 14:13:15 +0100

    module PromissoryNote: export UUID

--- src/PromissoryNote.hs
+++ src/PromissoryNote.hs
@@ -3,9 +3,11 @@ module PromissoryNote
     module PromissoryNote.Note
 ,   module PromissoryNote.StoredNote
 ,   module PromissoryNote.Redeem
+,   module PromissoryNote.Types.UUID
 )
  where
 
 import PromissoryNote.Note
 import PromissoryNote.StoredNote
 import PromissoryNote.Redeem
+import PromissoryNote.Types.UUID
